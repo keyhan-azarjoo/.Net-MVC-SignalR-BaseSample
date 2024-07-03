0f09b0f85df726982c9d417a0680c593839673ca
keyhan_azarjoo <keyhan.azarjoo@seamlessdigital.com>
2024-07-03 14:43:31 +0100

    connection protecols

--- wwwroot/js/usersCount.js
+++ wwwroot/js/usersCount.js
@@ -1,8 +1,31 @@
 ﻿// JS in Client Side
 
+// training video : https://www.youtube.com/watch?v=pl0OobPmWTk
+
+
 // Create connection
 // For connecting Client to server you need to create a connection like here
-var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
+var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount", signalR.HttpTransportType.WebSockets).build();
+// the transport type is WebSockets by defult
+// you can change the connection protecol here
+
+// Web Sockets
+// web Sockets is a 2 way connection.meanning that they create a connection and have it and use it when ever they want.
+// Client can send message to server and server can send message to client any time it want.
+
+// Server Sent Events
+// In this connection, client open a connection to server and server doesn't response to that and start intracting by client by events and send az much as event it want
+// it is not by directional. client use http request but the server use events to comunicate to clients
+
+// Long Polling
+// in this protecol, client send a request and server doesn't response to that and client wait to responce to the time out and after time out the client send another request and wait for the response again.
+// so client has a chanel which always is open.
+// when the server response to the client, client send another request so they are connected all the time.
+
+// you can check these protecols in the network tab in f12 in wevsite. 
+// you will see how connections open and close in different protecols
+
+
 
 
 
