6a446a53672596784b43281227d49d38c843e045
keyhan_azarjoo <keyhan.azarjoo@seamlessdigital.com>
2024-07-03 14:04:31 +0100

    adding code for different situations

--- Hubs/UserHub.cs
+++ Hubs/UserHub.cs
@@ -31,5 +31,87 @@ namespace SignalRSample.Hubs
             //send update to all clients that total views have been updated
             await Clients.All.SendAsync("updateTotalViews", TotalViews); // this function send a message to all clients. it invoke the updateTotalViews in client side
         }
+
+        // In the case client send a message and want the responce to all client inclouding it sellf
+        // A <=====> Server
+        // B <------ Server
+        // C <------ Server
+        // D <------ Server
+
+        // await Client.All.SendAsync("RecriveMessage", user, message)
+
+
+        // In  the case a client send a message, and want the response for just it self. Just it self
+        // A         Server
+        // B <=====> Server
+        // C         Server
+        // D         Server
+
+        // await Clients.Caller.SendAsync("ReceiveMessage", user, mnessage)
+
+
+
+        // in the case a client send a message and want the responce to anyone except it self. other client except the one that invoke the notification
+        // A ------> Server
+        // B <------ Server
+        // C <------ Server
+        // D <------ Server
+
+        // await Clients.Others.SendAsync("ReceiveMessage", users, message)
+
+
+        // In the case a client send a message and want the responce go to just anothe rclient. Like sending a message specifically to a user or browser. in here B send a message to A
+        // A <------ Server
+        // B ------> Server
+        // C         Server
+        // D         Server
+
+        // await Clients.Client("Connection Id - A").SendAsync("ReceiveMessage", user, message)
+
+
+
+        // In the case a user send a message to some other user. User B send a message and the responce goes to A and C
+        // A <------ Server
+        // B ------> Server
+        // C <------ Server
+        // D         Server
+
+        // await Clients.Clients("Connection Id - A" , "Connection Id - C").SendAsync("ReceiveMessage", user, message)
+
+
+
+        // In the case that user want to send a message to all users except user A and C.
+        // A         Server
+        // B <=====> Server
+        // C         Server
+        // D <------ Server
+        // E <------ Server
+        // F <------ Server
+
+        // await Clients.AllExcept("Connection Id - A", "Connection Id - C").SendAsync("ReceiveMessage", user, message)
+
+
+
+        // in the case the server wants to send a message to a client. send a notification for example. This client may have two open Tabs and we want to send messaage to all tabs. 
+        // ben@gmail.com A <------ Server
+        // ben@gmail.com B <------ Server
+        // sam@gmail.com C         Server
+        // sam@gmail.com D         Server
+        // don@gmail.com E         Server
+        // don@gmail.com F         Server
+
+        // await Client.User("ben@gmail.com").SendAsync("ReceiveMessage", user, message) // You can send it to a username or a Connection Id
+
+
+        // In the case we want to send notification to ben and don togater 
+        // ben@gmail.com B <------ Server
+        // sam@gmail.com C <------ Server
+        // sam@gmail.com D         Server
+        // don@gmail.com E <------ Server
+        // don@gmail.com F <------ Server
+        
+        // await Clients.Users("ben@gmail.com","don@gmail.com").SendAsync("ReceiveMessage", user, message) // in this case server send a message to two users and all of their open tabs.// you can use userId(email) and connection Id
+
+
     }
 }
