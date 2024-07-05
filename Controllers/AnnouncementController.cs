1da5764c58dd27d6b04df86b2c4cdde09f4bdcd1
keyhan_azarjoo <keyhan.azarjoo@seamlessdigital.com>
2024-07-05 14:18:48 +0100

    Sending message to all clients from controller by IHubContext

---
+++ Controllers/AnnouncementController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalRSample.Hubs;
+
+namespace SignalRSample.Controllers
+{
+    public class AnnouncementController : Controller
+    {
+        private IHubContext<MessagingServiceHub> _hubContext;
+        public AnnouncementController(IHubContext<MessagingServiceHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpGet("/announcement")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("/announcement")]
+        public async Task<IActionResult> Post([FromForm] string message)
+        {
+            await _hubContext.Clients.All.SendAsync("ReceiveMessage", message);
+            return RedirectToAction("Index");
+
+        }
+    }
+}
